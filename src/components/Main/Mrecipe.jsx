220927da431a1080bdac606bb6838815ec170087
Anjali Saroj <151427345+hiAnjali@users.noreply.github.com>
2025-07-09 17:55:00 +0530

    Add files via upload

---
+++ src/components/Main/Mrecipe.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Mrecipe.css';
+
+const Mrecipe = () => {
+  return (
+    <div className='m-recipe container'>
+        <div className="m-recipe-text">
+            <h1>Discover the Flavors of India</h1>
+            <p>Explore authentic recipes, from spicy street food to comforting home-cooked meals. Your culinary journey starts here.</p>
+            <button className='btn'>Explore More</button>
+        </div>
+    </div>
+  );
+};
+
+export default Mrecipe;
